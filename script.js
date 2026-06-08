d9fb5fe11082a23102b4c4b4a76862ac494beef8
maysabubaker16-commits <maysabubaker16@gmail.com>
2026-06-07 18:26:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+   let darkBtn = document.getElementById("darkBtn");
+
+  darkBtn.onclick = function(){
+  document.body.classList.toggle("dark");
+ }
+
+
+   function welcome(){
+  alert("welcome to my protofolio");
+}
